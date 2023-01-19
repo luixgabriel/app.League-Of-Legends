c1c6aff2243b1ddff570e25706607b713fb70800
luix gabriel <luisgasbriel@gmail.com>
2023-01-19 12:51:16 -0300

    Adicionando async e await

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -21,20 +21,6 @@ class HomeController {
 
     const playerData = await (player.json());
     res.json(playerData);
-
-    //   fetch(`https://br1.api.riotgames.com/tft/league/v1/entries/by-summoner/${idLol}`, {
-    //     headers: {
-    //       'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //       'X-Riot-Token': 'RGAPI-e602468d-707f-4b7a-93a2-069755b8ceaa',
-    //     },
-    //   })
-    //     .then((response2) => response2.json())
-    //     .then((data2) => {
-    //       console.log(data2);
-    //     });
-    // });
-
-    // https://br1.api.riotgames.com/tft/league/v1/entries/by-summoner/R-U27LQQRdxRg9_B9yh4uibG7kPqr2FxN7q6IGeyMNIje64
   }
 }
 export default new HomeController();
