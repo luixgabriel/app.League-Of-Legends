de0d4637ac2c026e20566aa636c57c1b9dae8509
luix gabriel <luisgasbriel@gmail.com>
2023-01-23 09:58:37 -0300

    Adicionando DOTENV

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 import router from './routes';
 
 const app = express();
 // app.use(cors);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(router);
 
 app.listen(8087, () => {
--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,22 +1,26 @@
-import fetch from 'node-fetch';
+import fetch, { Headers } from 'node-fetch';
 
 class HomeController {
+  // async index(req, res) {
+  //   return res.render('index');
+  // }
   async index(req, res) {
-    const response = await fetch('https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/zeepxanflorp', {
-      headers: {
-        'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'X-Riot-Token': 'RGAPI-e602468d-707f-4b7a-93a2-069755b8ceaa',
-      },
+    const headers = {
+      'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'X-Riot-Token': process.env.APIKEY,
+    };
+
+    const name = 'luixgabriel';
+
+    const response = await fetch(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`, {
+      headers,
     });
 
     const data = await (response.json());
     const idLol = data.id;
 
     const player = await fetch(`https://br1.api.riotgames.com/tft/league/v1/entries/by-summoner/${idLol}`, {
-      headers: {
-        'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'X-Riot-Token': 'RGAPI-e602468d-707f-4b7a-93a2-069755b8ceaa',
-      },
+      headers,
     });
 
     const playerData = await (player.json());
