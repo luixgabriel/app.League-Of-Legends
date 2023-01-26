ea98ee7b1735e01678ac5fbc5e1b80ba96023627
luix gabriel <luisgasbriel@gmail.com>
2023-01-26 08:45:38 -0300

    Deploy

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
@@ -6,7 +6,11 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
-import router from './routes';
+// import router from './routes';
+
+import HomeController from './controllers/HomeController';
+
+const router = new Router();
 
 const app = express();
 // app.use(cors);
@@ -15,6 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(router);
+
+router.get('/', HomeController.index);
+router.post('/player', HomeController.playerData);
 
 const port = process.env.PORT;
 
